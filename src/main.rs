52f6065edf4515839fc25ebb718e95f0080b89c8
887 <2300887@gmail.com>
2016-08-23 22:23:30 +0200

    Parsing

--- src/main.rs
+++ src/main.rs
@@ -29,18 +29,45 @@ pub fn main() {
 
     let reader = io::stdin();
     let mut result = String::new();
-    println!("input:");
-    //let bytes_read = reader.read_line(&mut result).unwrap();
-    reader.read_line(&mut result).ok();
-    println!("output:");
+
+    //println!("input:");
+    ////let bytes_read = reader.read_line(&mut result).unwrap();
+    //reader.read_line(&mut result).ok();
+
+    println!("enter equation:");
     println!("{}",result);
 
     result.clear();
     reader.read_line(&mut result).ok();
 
     let mut last_op = Op::None;
-    let x = result.split_whitespace().fold(0, |mut res: isize, s: &str| {
-        match s {
+
+    //let parsed = result.split_whitespace();
+    //let x = parsed.iter().fold(0, |mut res: isize, s: &str| {
+    //  match s {
+
+    let parsed = result
+        .chars()
+        .fold(vec![String::new()], |mut res, chr|{
+            match chr {
+                '+' | '-' | '*' | '/' => {
+                    println!("action: {}", chr);
+                    res.push(chr.to_string());
+                    res.push(String::new());
+                },
+                ' ' | '\r' | '\n' => {
+                    //do_nothing
+                },
+                _ => {
+                    println!("value: {}", chr);
+                    res.last_mut().unwrap().push(chr);
+                },
+            }
+            res
+        });
+
+    let x = parsed.iter().fold(0, |mut res: isize, s: &String| {
+        match (*s).as_str() {
             "+" => last_op = Op::Plus,
             "-" => last_op = Op::Minus,
             "*" => last_op = Op::Multiply,
@@ -62,4 +89,10 @@ pub fn main() {
 
     let path: &'static str = env!("PATH");
     println!("the $PATH variable at the time of compiling was: {}", path);
+
+    let key = "PATH";
+    match env::var(key) {
+        Ok(val) => println!("the ${} variable at runtime was: : {:?}", key, val),
+        Err(e) => println!("couldn't interpret {}: {}", key, e),
+    }
 }
